e04204af9df9c6e884538b591100254dd38b047a
miguelPretell12 <71430564+miguelPretell12@users.noreply.github.com>
2022-03-18 17:37:53 -0500

    Se añadio mas interactiva en la pagina JS

--- src/js/script.js
+++ src/js/script.js
@@ -13,7 +13,32 @@ const navheader = document.querySelector(".enlace-header");
 // Arreglo
 let pedidos = [];
 
-cantidadCarrito.textContent = `${pedidos.length}`;
+// Objeto
+let pedido = {};
+
+// Total
+const spanTotal = document.querySelector("#total");
+
+// Cargar localStorage
+document.addEventListener("DOMContentLoaded", function () {
+  pedidos = JSON.parse(localStorage.getItem("pedidos")) || [];
+  cantidadCarrito.textContent = `${pedidos.length}`;
+  carritoPedido();
+  totalCarrito();
+  d();
+});
+
+function d() {
+  btnAgregar.forEach((btn) => {
+    pedidos.forEach((p) => {
+      const { id } = p;
+      if (id == btn.dataset.id) {
+        btn.classList.remove("fa-plus", "background-red");
+        btn.classList.add("fa-check", "background-green");
+      }
+    });
+  });
+}
 
 window.addEventListener("scroll", function (e) {
   let scroll = window.scrollY;
@@ -56,14 +81,27 @@ btnAgregar.forEach((btn) => {
       titulo: e.target.parentElement.querySelector(".card-info .title-card")
         .textContent,
       id: e.target.dataset.id,
+      clase1: e.target.classList[2],
+      clase2: e.target.classList[3],
     };
 
-    const existe = pedidos.some((p) => p.id == pedido.id);
-    if (existe) {
-      return;
+    if (e.target.classList.contains("fa-plus")) {
+      e.target.classList.remove("fa-plus", "background-red");
+      e.target.classList.add("fa-check", "background-green");
+      const existe = pedidos.some((p) => p.id == pedido.id);
+      if (existe) {
+        return;
+      } else {
+        pedidos = [...pedidos, pedido];
+        cantidadCarrito.textContent = `${pedidos.length}`;
+      }
     } else {
-      pedidos = [...pedidos, pedido];
+      e.target.classList.remove("fa-check", "background-green");
+      e.target.classList.add("fa-plus", "background-red");
+
+      pedidos = pedidos.filter((pd) => pd.id != e.target.dataset.id);
       cantidadCarrito.textContent = `${pedidos.length}`;
+      totalCarrito();
     }
     carritoPedido();
   });
@@ -77,6 +115,7 @@ function limpiarHTML() {
 
 function carritoPedido() {
   limpiarHTML();
+  totalCarrito();
   pedidos.forEach((p) => {
     const { img, precio, id, cantidad, titulo } = p;
     const div = document.createElement("div");
@@ -89,20 +128,81 @@ function carritoPedido() {
         </div>
         <p>${titulo}</p>
         <p class="precio">Precio: <span>${precio}</span></p>
-        <p>Cantidad: <input type="number" value="${cantidad}"></p>
+        <p>Cantidad: <input type="number" class="input-50" min="1" max="10" id="cantidad" data-id="${id}" value="${cantidad}"></p>
     </div>
     `;
     contenedorCarrito.appendChild(div);
   });
   eliminar();
+  sincronizarStorage();
+  cambiaCantidad();
 }
 
 function eliminar() {
   const btnEliminar = document.querySelectorAll(".btn-eliminar");
   btnEliminar.forEach((e) => {
     e.addEventListener("click", (p) => {
+      btnAgregar.forEach((btn) => {
+        if (btn.dataset.id == p.target.dataset.id) {
+          btn.classList.remove("fa-check", "background-green");
+          btn.classList.add("fa-plus", "background-red");
+        }
+      });
       pedidos = pedidos.filter((pd) => pd.id != p.target.dataset.id);
+      cantidadCarrito.textContent = `${pedidos.length}`;
       carritoPedido();
+      totalCarrito();
     });
   });
+}
+
+// LocalStorage
+function sincronizarStorage() {
+  localStorage.setItem("pedidos", JSON.stringify(pedidos));
+}
+
+function cambiaCantidad() {
+  const cantidades = document.querySelectorAll("#cantidad");
+
+  let subtotal;
+
+  cantidades.forEach((cantidad) => {
+    cantidad.addEventListener("keyup", function (e) {
+      let total = 0;
+      if (pedidos.length > 0) {
+        pedidos.forEach((pe, a) => {
+          if (pe.id == e.target.dataset.id) {
+            pe.cantidad = e.target.value;
+          }
+          subtotal = pe.cantidad * pe.precio;
+          total += subtotal;
+          for (let i = 0; i < a + 1; i++) {
+            if (i == a) {
+              spanTotal.textContent = total;
+            }
+          }
+        });
+      } else {
+        spanTotal.textContent = total;
+      }
+    });
+  });
+}
+
+function totalCarrito() {
+  let subtotal;
+  let total = 0;
+  if (pedidos.length > 0) {
+    pedidos.forEach((pe, a) => {
+      subtotal = pe.cantidad * pe.precio;
+      total += subtotal;
+      for (let i = 0; i < a + 1; i++) {
+        if (i == a) {
+          spanTotal.textContent = total;
+        }
+      }
+    });
+  } else {
+    spanTotal.textContent = total;
+  }
 }
